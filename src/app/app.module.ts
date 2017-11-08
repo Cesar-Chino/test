280c036d945d7732d4ee5bcc8367ff303ebbd71f
Cesar-Chino <cesaruriel007@hotmail.com>
2017-11-08 12:44:17 -0600

    hola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Footer.component.tsComponent } from './footer.component.ts/footer.component.ts.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    Footer.component.tsComponent,
+    FooterComponent
+],
   imports: [
     BrowserModule
   ],
