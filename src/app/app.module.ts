2ebb20451f4646e43e9ede1e73390c91b420f617
Cesar-Chino <cesaruriel007@hotmail.com>
2017-11-08 14:14:15 -0600

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { FooterComponent } from './footer/footer.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { Footer.component.tsComponent } from './footer.component.ts/footer.component.ts.component';
-import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Footer.component.tsComponent,
     FooterComponent
 ],
   imports: [
